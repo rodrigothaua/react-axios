6e56da6344f640e52c9b187832afec2932d91d3f
Rodrigo Thauã Lima Barroso <rodrigothaua@gmail.com>
2022-12-01 15:55:54 -0400

    home, post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+//Navbar
+import Navbar from './components/Navbar'
+
+import { Outlet } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>React Axios</h1>
+      <Navbar/>
+      <div className='container'>
+        <Outlet />
+      </div>
     </div>
   )
 }
